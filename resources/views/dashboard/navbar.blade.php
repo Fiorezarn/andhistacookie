f9d046e304aebe035e62f26765fe832e34af9258
fiorezarn <fioreza1902@gmail.com>
2023-07-06 08:55:22 +0700

    feat: admin side

---
+++ resources/views/dashboard/navbar.blade.php
@@ -0,0 +1,30 @@
+<ul class="sidebar-menu" data-widget="tree">
+    <li class="header">MAIN NAVIGATION</li>
+    <li><a href="/"><i class="fa fa-dashboard"></i> <span>Home</span></a></li>
+    <li class="treeview">
+      <a href="#">
+        <i class="fa fa-share"></i> <span>Product</span>
+        <span class="pull-right-container">
+          <i class="fa fa-angle-left pull-right"></i>
+        </span>
+      </a>
+      <ul class="treeview-menu">
+        <li><a href="/v_toner"><i class="fa fa-circle-o"></i> <span>Toner</span></a></li>
+        <li><a href="/v_serum"><i class="fa fa-circle-o"></i> <span>Serum</span></a></li>
+        <li><a href="/v_moisturizer"><i class="fa fa-circle-o"></i> <span>Moisturizer</span></a></li>
+      </ul>
+    </li>
+    <li class="treeview">
+      <a href="#">
+        <i class="fa fa-share"></i> <span>Review</span>
+        <span class="pull-right-container">
+          <i class="fa fa-angle-left pull-right"></i>
+        </span>
+      </a>
+      <ul class="treeview-menu">
+        <li><a href="/review_toner"><i class="fa fa-circle-o"></i> <span>Review Toner</span></a></li>
+        <li><a href="/review_serum"><i class="fa fa-circle-o"></i> <span>Review Serum</span></a></li>
+        <li><a href="/review_moisturizer"><i class="fa fa-circle-o"></i> <span>Review Moisturizer</span></a></li>
+      </ul>
+    </li>
+  </ul>
